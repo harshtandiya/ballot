8442e73eea02bba8994e2ca3de866644f4e722cd
harshtandiya <harsh.tandiya@gmail.com>
2024-09-25 01:16:38 +0530

    feat: Add `Election Nomination Form` doctype and related files

---
+++ ballot/election/doctype/election_nomination_form/election_nomination_form.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Harsh Tandiya and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Election Nomination Form", {
+// 	refresh(frm) {
+
+// 	},
+// });
