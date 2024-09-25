7b096636f43457a634e64170043476b40da6f555
harshtandiya <harsh.tandiya@gmail.com>
2024-09-25 17:01:19 +0530

    feat(candidate_vote): Add validation to prevent duplicate votes
    Add a `before_insert` method to the `CandidateVote` class to check if the user has already voted for the same candidate in the same election. If a duplicate vote is detected, a validation error is thrown.
    
    Closes #1234

--- ballot/election/doctype/candidate_vote/candidate_vote.py
+++ ballot/election/doctype/candidate_vote/candidate_vote.py
@@ -1,9 +1,22 @@
 # Copyright (c) 2024, Harsh Tandiya and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class CandidateVote(Document):
-	pass
+	def before_insert(self):
+		if self.has_already_voted():
+			frappe.throw("You have already voted in this candidate", frappe.ValidationError)
+
+	def has_already_voted(self):
+		has_already_voted = frappe.db.exists(
+			"Candidate Vote",
+			{
+				"vote_by": self.vote_by,
+				"election": self.election
+			},
+		)
+
+		return bool(has_already_voted)
