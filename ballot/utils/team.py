5752e862ef4ea66a0d64966cc3bb0e9ad49e5200
harshtandiya <harsh.tandiya@gmail.com>
2024-09-25 16:50:46 +0530

    chore(utils): Add `is_election_team_member` method
    This method checks whether a user is a member of a given election team

---
+++ ballot/utils/team.py
@@ -0,0 +1,22 @@
+import frappe
+
+def is_election_team_member(election: str, user: str):
+    '''
+    Check if the user is a member of the election's team
+
+    Args:
+        election (str): Election ID
+        user (str): User ID
+    
+    Returns:
+        bool: True if the user is a member of the election's team, False otherwise
+    '''
+    team_id = frappe.db.get_value("Election", election, "organizing_team")
+
+    is_team_member = frappe.db.exists("Election Team Member", {
+        "parent": team_id,
+        "parenttype": "Election Team",
+        "user": user
+    })
+
+    return bool(is_team_member)
