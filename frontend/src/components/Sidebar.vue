ebdc83f72d40715e0ab9aff8149206537b1314aa
harshtandiya <harsh.tandiya@gmail.com>
2024-10-07 15:21:47 +0530

    feat: Add Sidebar component
    This commit adds a new component called Sidebar.vue. The Sidebar component is responsible for rendering a sidebar menu with various options. It includes a logo, menu items, and user details section with options like My Profile and Logout.
    
    The Sidebar component uses the `tabler/icons-vue` package to render the user icon. It also uses the `frappe-ui` library for creating a popover and the `vue-router` library for navigation.

---
+++ frontend/src/components/Sidebar.vue
@@ -0,0 +1,113 @@
+<template>
+  <div
+    class="relative block min-h-0 flex-shrink-0 overflow-hidden hover:overflow-auto"
+  >
+    <div
+      class="fixed flex justify-between min-h-screen w-[220px] flex-col border-r bg-gray-50 p-4"
+    >
+      <div class="flex flex-col gap-4">
+        <div class="mb-3">
+          <div class="font-sans text-2xl font-semibold">Ballot.</div>
+        </div>
+        <div class="flex flex-col gap-1">
+          <Button
+            v-for="(item, index) in sidebarItems"
+            :key="item.label"
+            :label="item.label"
+            :variant="isMenuItemActive(item.route, index) ? 'subtle' : 'ghost'"
+            :class="
+              isMenuItemActive(item.route, index)
+                ? 'bg-gray-200 font-medium'
+                : ''
+            "
+            size="sm"
+            class="!justify-start text-sm rounded-sm"
+            @click="router.push(item.route)"
+          />
+        </div>
+      </div>
+      <div class="flex flex-col gap-4">
+        <div class="flex items-center justify-between text-gray-800">
+          <div class="flex items-center gap-2">
+            <img
+              v-if="userDetails.data?.user_image"
+              :src="userDetails.data?.user_image"
+              class="w-6 h-6 rounded-full"
+            />
+            <IconUserFilled v-else class="w-3 h-3" />
+            <span class="text-sm font-medium">{{
+              userDetails.data?.full_name
+            }}</span>
+          </div>
+          <div>
+            <Popover>
+              <template #target="{ togglePopover }">
+                <Button
+                  icon="more-vertical"
+                  variant="ghost"
+                  @click="togglePopover()"
+                />
+              </template>
+              <template #body-main>
+                <div class="flex flex-col gap-1 p-2">
+                  <Button
+                    class="!justify-start text-sm rounded-sm cursor-pointer"
+                    label="My Profile"
+                    :link="getRedirectUrl('me')"
+                    variant="ghost"
+                  />
+                  <Button
+                    variant="ghost"
+                    theme="red"
+                    class="!justify-start text-sm rounded-sm cursor-pointer"
+                    label="Logout"
+                    @click="session.logout.fetch()"
+                  />
+                </div>
+              </template>
+            </Popover>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import { IconUserFilled } from '@tabler/icons-vue'
+import { createResource, Popover } from 'frappe-ui'
+import { inject } from 'vue'
+import { getRedirectUrl } from '@/utils/helpers'
+import { useRoute, useRouter } from 'vue-router'
+
+const session = inject('$session')
+const route = useRoute()
+const router = useRouter()
+
+const props = defineProps({
+  sidebarItems: {
+    type: Array,
+    required: true,
+  },
+})
+
+const userDetails = createResource({
+  url: 'ballot.api.user.get_user_details',
+  makeParams() {
+    return {
+      user: session.user,
+    }
+  },
+  auto: true,
+})
+
+const isMenuItemActive = (menuRoute, index) => {
+  if (index == 0 && menuRoute != route.path) {
+    return false
+  }
+  return (
+    menuRoute === route.path ||
+    menuRoute ===
+      '/' + route.path.split('/').filter(Boolean).slice(0, -1).join('/')
+  )
+}
+</script>
