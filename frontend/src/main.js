df3875de1cf994f6f1ec476994b67ce8b7c6805a
harshtandiya <harsh.tandiya@gmail.com>
2024-10-09 00:47:59 +0530

    FEAT: add `primevue` ui framework to the project

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,10 @@ import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
 
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import { definePreset } from '@primevue/themes'
+
 import {
   Button,
   Card,
@@ -19,6 +23,32 @@ setConfig('resourceFetcher', frappeRequest)
 
 app.use(router)
 app.use(resourcesPlugin)
+
+const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{zinc.50}',
+      100: '{zinc.100}',
+      200: '{zinc.200}',
+      300: '{zinc.300}',
+      400: '{zinc.400}',
+      500: '{zinc.500}',
+      600: '{zinc.600}',
+      700: '{zinc.700}',
+      800: '{zinc.800}',
+      900: '{zinc.900}',
+      950: '{zinc.950}',
+    },
+  },
+})
+app.use(PrimeVue, {
+  theme: {
+    preset: MyPreset,
+    options: {
+      darkModeSelector: false,
+    },
+  },
+})
 
 app.component('Button', Button)
 app.component('Card', Card)
