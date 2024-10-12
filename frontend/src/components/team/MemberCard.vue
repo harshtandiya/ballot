0db18d77f65d163082ce1974e5dee9426ea9fd6e
harshtandiya <harsh.tandiya@gmail.com>
2024-10-13 00:59:14 +0530

    feat: add MemberCard.vue

---
+++ frontend/src/components/team/MemberCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <Card class="!shadow-none border border-primary-200">
+    <template #header>
+      <div class="ml-4 mt-4 font-medium font-sans">
+        <Avatar
+          v-if="member.user_image"
+          :image="member.user_image"
+          shape="circle"
+        />
+        <Avatar v-else :label="member.full_name.charAt(0)" shape="circle" />
+      </div>
+    </template>
+    <template #content>{{ member.full_name }}</template>
+  </Card>
+</template>
+<script setup>
+import Avatar from 'primevue/avatar'
+import Card from 'primevue/card'
+
+defineProps({
+  member: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
