f715d1420478b4e208beb5e1b9722ae9b3b67c05
harshtandiya <harsh.tandiya@gmail.com>
2024-10-18 12:58:55 +0530

    chore: better sidebar for team and election dashboard

--- frontend/src/pages/TeamDashboard.vue
+++ frontend/src/pages/TeamDashboard.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="flex">
-    <Sidebar :sidebar-items="sidebarItems" />
+    <Sidebar :sidebar-items="sidebarItems">
+      <template #pre-nav-items>
+        <div>
+          <Button
+            label="Go Home"
+            icon-left="arrow-left"
+            variant="ghost"
+            :route="`/`"
+          />
+        </div>
+        <div>
+          <h4 class="text-sm uppercase mb-0 font-semibold">Manage Team</h4>
+          <hr class="my-2" />
+          <span v-if="team.doc" class="text-base">{{
+            team.doc.team_name
+          }}</span>
+        </div>
+      </template>
+    </Sidebar>
     <div class="w-full md:ml-[220px]">
       <RouterView />
     </div>
--- frontend/src/pages/election/ElectionDashboard.vue
+++ frontend/src/pages/election/ElectionDashboard.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="flex">
-    <Sidebar :sidebar-items="sidebarItems" />
+    <Sidebar :sidebar-items="sidebarItems">
+      <template #pre-nav-items>
+        <div>
+          <Button
+            v-if="election.data"
+            label="Go Back"
+            icon-left="arrow-left"
+            variant="ghost"
+            :route="`/my-team/${election.data.organizing_team}/elections`"
+          />
+        </div>
+        <div>
+          <h4 class="text-sm uppercase mb-0 font-semibold">Manage Election</h4>
+          <hr class="my-2" />
+          <span v-if="election.data" class="text-base">{{
+            election.data.title
+          }}</span>
+        </div>
+      </template>
+    </Sidebar>
     <div class="w-full md:ml-[220px]">
       <RouterView />
     </div>
@@ -9,8 +28,20 @@
 <script setup>
 import Sidebar from '@/components/Sidebar.vue'
 import { useRoute } from 'vue-router'
+import { createResource } from 'frappe-ui'
 
 const route = useRoute()
+
+const election = createResource({
+  url: 'frappe.client.get',
+  makeParams() {
+    return {
+      doctype: 'Election',
+      name: route.params.id,
+    }
+  },
+  auto: true,
+})
 
 const sidebarItems = [
   {
@@ -18,7 +49,7 @@ const sidebarItems = [
     route: `/my-elections/${route.params.id}`,
   },
   {
-    label: 'Nominations',
+    label: 'Manage Candidates',
     route: `/my-elections/${route.params.id}/nomination`,
   },
 ]
