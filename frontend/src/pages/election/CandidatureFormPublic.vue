82abd2ee11cbe7a2aa210491845a343170d29250
harshtandiya <harsh.tandiya@gmail.com>
2024-10-18 15:46:58 +0530

    feat:(candidate-form) render form based on status

--- frontend/src/pages/election/CandidatureFormPublic.vue
+++ frontend/src/pages/election/CandidatureFormPublic.vue
@@ -2,17 +2,35 @@
   <Header />
   <div class="flex w-full justify-center py-6">
     <div class="space-y-4 w-full max-w-screen-xl px-6">
-      <h1 class="text-3xl font-sans font-semibold">Apply for Candidature</h1>
-      <Divider />
-      <div v-if="form.loading">
+      <div v-if="formStatus == 'Loading'">
         <LoadingText />
       </div>
+      <div v-else-if="formStatus == 'Live'">
+        <h1 class="text-3xl font-sans font-semibold">Apply for Candidature</h1>
+        <Divider />
+        <div v-if="form.loading">
+          <LoadingText />
+        </div>
+        <div v-else>
+          <RenderForm
+            v-model:fields="fields_meta"
+            :election="election.data"
+            :form="form.data"
+          ></RenderForm>
+        </div>
+      </div>
       <div v-else>
-        <RenderForm
-          v-model:fields="fields_meta"
-          :election="election.data"
-          :form="form.data"
-        ></RenderForm>
+        <div
+          class="w-full h-[320px] flex gap-2 flex-col justify-center items-center"
+        >
+          <IconAlertCircle size="2rem" />
+          <h1 class="text-xl font-semibold">
+            This candidature form is not live!
+          </h1>
+          <p class="text-base text-primary-600">
+            Please contact organizing team members for more info.
+          </p>
+        </div>
       </div>
     </div>
   </div>
@@ -24,9 +42,10 @@ import { createResource, LoadingText } from 'frappe-ui'
 import { useRoute } from 'vue-router'
 import RenderForm from '@/components/form/RenderForm.vue'
 import { ref } from 'vue'
+import { IconAlertCircle } from '@tabler/icons-vue'
 
 const route = useRoute()
-
+const formStatus = ref('Loading')
 const fields_meta = ref([])
 
 const election = createResource({
@@ -57,7 +76,10 @@ const form = createResource({
   },
   loading: true,
   onSuccess(data) {
-    fields_meta.value = JSON.parse(data.fields_meta)
+    formStatus.value = data.status
+    if (data.status === 'Live') {
+      fields_meta.value = JSON.parse(data.fields_meta)
+    }
   },
 })
 </script>
