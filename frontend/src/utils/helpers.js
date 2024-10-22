fc8af866f7cf4ffa63fa3b2bd78cf2fe9eddb846
harshtandiya <harsh.tandiya@gmail.com>
2024-10-23 00:38:46 +0530

    feat: add `getRelativeTime` method

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -1,7 +1,27 @@
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import isToday from 'dayjs/plugin/isToday'
+import isYesterday from 'dayjs/plugin/isYesterday'
+
+dayjs.extend(relativeTime)
+dayjs.extend(isToday)
+dayjs.extend(isYesterday)
+
 export const getRedirectUrl = (route) => {
   return window.location.origin + '/' + route
 }
 
 export const generateRandomId = () => {
   return Math.random().toString(36).slice(2, 8)
+}
+
+export const getRelativeTime = (datetime) => {
+  const date = dayjs(datetime)
+  if (date.isToday()) {
+    return 'Today'
+  } else if (date.isYesterday()) {
+    return 'Yesterday'
+  } else {
+    return date.fromNow()
+  }
 }
