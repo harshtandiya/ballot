5d3c469f6c21ccc85bd95b88a0b492ece9fe274f
harshtandiya <harsh.tandiya@gmail.com>
2024-10-22 14:22:27 +0530

    feat: Add Candidate Card component

---
+++ frontend/src/components/candidature/CandidateCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <Card
+    class="!shadow-none !border hover:border-primary-700 hover:cursor-pointer transition-colors"
+    @click="
+      router.push({
+        name: 'Candidature Submission Public',
+        params: { slug: route.params.slug, id: candidate.name },
+      })
+    "
+  >
+    <template #content>
+      <div class="flex gap-3 items-start">
+        <Avatar v-if="candidate.photo" :image="candidate.photo" />
+        <Avatar v-else :label="candidate.full_name.charAt(0)" shape="" />
+        <div class="flex flex-col gap-1">
+          <h5 class="text-lg font-medium">{{ candidate.full_name }}</h5>
+          <div class="text-sm">
+            {{ candidate.designation }} at {{ candidate.organization }}
+          </div>
+        </div>
+      </div>
+    </template>
+  </Card>
+</template>
+<script setup>
+import Card from 'primevue/card'
+import Avatar from 'primevue/avatar'
+import { useRouter, useRoute } from 'vue-router'
+
+const router = useRouter()
+const route = useRoute()
+
+defineProps({
+  candidate: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
--- frontend/src/pages/election/ElectionHome.vue
+++ frontend/src/pages/election/ElectionHome.vue
@@ -27,12 +27,30 @@
           :has-form="hasCandidateForm.data"
         />
       </div>
+      <div v-if="candidates.data" class="mt-4 space-y-2">
+        <h2 class="text-xl font-semibold">Candidates</h2>
+        <hr />
+        <p
+          v-if="candidates.data.length == 0"
+          class="text-base pt-2 text-primary-700"
+        >
+          No candidates for this election yet.
+        </p>
+        <div v-else class="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <CandidateCard
+            v-for="candidate in candidates.data"
+            :key="candidate.name"
+            :candidate="candidate"
+          />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <script setup>
 import Header from '@/components/Header.vue'
-import CandidateFormStatusBanner from './CandidateFormStatusBanner.vue'
+import CandidateFormStatusBanner from '@/components/candidature/CandidateFormStatusBanner.vue'
+import CandidateCard from '@/components/candidature/CandidateCard.vue'
 import { createResource } from 'frappe-ui'
 import { useRoute } from 'vue-router'
 
@@ -48,6 +66,7 @@ const election = createResource({
   auto: true,
   onSuccess() {
     hasCandidateForm.fetch()
+    candidates.fetch()
   },
 })
 
@@ -69,6 +88,15 @@ const hasCandidateForm = createResource({
 
 const candidateForm = createResource({
   url: 'ballot.api.election.get_election_candidature_form',
+  makeParams() {
+    return {
+      election: election.data.name,
+    }
+  },
+})
+
+const candidates = createResource({
+  url: 'ballot.api.candidate.get_all_candidates',
   makeParams() {
     return {
       election: election.data.name,
