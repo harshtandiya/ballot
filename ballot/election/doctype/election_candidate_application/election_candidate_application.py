c56f152655bec2a1492d21567778ffab96dc706b
harshtandiya <harsh.tandiya@gmail.com>
2024-10-23 16:45:44 +0530

    chore(candidate-application): controller hooks for handing `accept_incoming` in form

--- ballot/election/doctype/election_candidate_application/election_candidate_application.py
+++ ballot/election/doctype/election_candidate_application/election_candidate_application.py
@@ -8,6 +8,22 @@ from ballot.utils.team import is_election_team_member
 
 
 class ElectionCandidateApplication(Document):
+    def before_insert(self):
+        if self.status != "Pending":
+            frappe.throw(
+                "You can only create a candidate application with status 'Pending'",
+                frappe.PermissionError,
+            )
+
+    def after_insert(self):
+        accept_incoming = frappe.db.get_value(
+            "Election Nomination Form", self.nomination_form, "accept_incoming_applications"
+        )
+
+        if accept_incoming:
+            self.status = 'Accepted'
+            self.save(ignore_permissions=True)
+
     def before_save(self):
         if (
             self.modified_by != self.owner
@@ -17,3 +33,17 @@ class ElectionCandidateApplication(Document):
             frappe.throw(
                 "You do not have the permission to edit this document", frappe.PermissionError
             )
+
+        if self.has_value_changed("status"):
+            if self.status == "Accepted":
+                self.create_candidate()
+
+    def create_candidate(self):
+        candidate = frappe.get_doc(
+            {
+                "doctype": "Election Candidate",
+                "election": self.election,
+                "linked_application": self.name,
+            }
+        )
+        candidate.insert(ignore_permissions=True)
