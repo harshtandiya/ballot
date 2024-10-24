a519fba01d64bcd2722b5a06f46654792f1fb167
harshtandiya <harsh.tandiya@gmail.com>
2024-10-24 14:30:59 +0530

    chore: add `go to dashboard` link to public header

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="w-full px-5 py-3 border-b">
+  <div class="w-full px-5 py-3 border-b flex justify-between">
     <h2 class="text-2xl font-sans font-bold">Ballot.</h2>
+    <a
+      href="/ballot"
+      class="text-base flex items-center gap-1 text-primary-800 hover:text-primary-600 hover:cursor-pointer"
+    >
+      Go to ballot dashboard <IconArrowRight size="1rem" />
+    </a>
   </div>
 </template>
-<script setup></script>
+<script setup>
+import { IconArrowRight } from '@tabler/icons-vue'
+</script>
