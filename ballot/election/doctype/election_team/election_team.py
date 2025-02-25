65dc4f5bc680babb9c6ad8eb95ae6f9444c570ef
harshtandiya <harsh.tandiya@gmail.com>
2025-02-25 14:24:15 +0530

    feat!: add sharing workflow to election team

--- ballot/election/doctype/election_team/election_team.py
+++ ballot/election/doctype/election_team/election_team.py
@@ -16,6 +16,10 @@ class ElectionTeam(Document):
         """Handle member role assignment and removal."""
         self.add_team_member_role()
         self.remove_team_member_role()
+        self.manage_docshare_on_save()
+
+    def after_insert(self):
+        self.manage_docshare_on_insert()
 
     def add_team_member_role(self):
         """Ensure all team members have the required role."""
@@ -44,9 +48,7 @@ class ElectionTeam(Document):
 
     def user_in_other_teams(self, user):
         """Check if the user is part of other Election Teams."""
-        return frappe.db.exists(
-            self.ROLE, {"user": user, "parent": ("!=", self.name)}
-        )
+        return frappe.db.exists(self.ROLE, {"user": user, "parent": ("!=", self.name)})
 
     def remove_role_from_user(self, user):
         """Remove the role from a user if they are not part of any other teams."""
@@ -56,3 +58,54 @@ class ElectionTeam(Document):
         if self.ROLE in role_map:
             user_doc.get("roles").remove(role_map[self.ROLE])
             user_doc.save(ignore_permissions=True)
+
+    def manage_docshare_on_save(self):
+        """Manage DocShare for team members, adding and removing as needed."""
+        if self.is_new():
+            return
+
+        prev_doc = self.get_doc_before_save()
+        current_users = {member.user for member in self.members}
+        prev_users = {member.user for member in prev_doc.members} if prev_doc else set()
+
+        # Add DocShare for new members
+        new_users = current_users - prev_users
+        for user in new_users:
+            self.add_docshare(user)
+
+        # Remove DocShare for removed members
+        removed_users = prev_users - current_users
+        for user in removed_users:
+            self.remove_docshare(user)
+
+    def manage_docshare_on_insert(self):
+        """Manage DocShare creation for members at time of Team creation"""
+        current_users = {member.user for member in self.members}
+
+        for user in current_users:
+            self.add_docshare(user)
+
+    def add_docshare(self, user):
+        """Create a DocShare entry if it doesn't already exist."""
+        if not frappe.db.exists(
+            "DocShare", {"share_doctype": "Election Team", "share_name": self.name, "user": user}
+        ):
+            docshare = frappe.get_doc(
+                {
+                    "doctype": "DocShare",
+                    "share_doctype": "Election Team",
+                    "share_name": self.name,
+                    "user": user,
+                    "read": 1,
+                    "write": 1,
+                    "share": 1,
+                }
+            )
+            docshare.flags.ignore_share_permission = 1
+            docshare.insert(ignore_permissions=True)
+
+    def remove_docshare(self, user):
+        """Delete the DocShare entry for the removed member."""
+        frappe.db.delete(
+            "DocShare", {"share_doctype": "Election Team", "share_name": self.name, "user": user}
+        )
