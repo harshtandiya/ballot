49798fca6487d233976cd8a798d098b5a1eb1a60
harshtandiya <harsh.tandiya@gmail.com>
2025-02-25 12:14:14 +0530

    feat: add doctype id constants file

---
+++ ballot/id/doctypes.py
@@ -0,0 +1,11 @@
+# Constants for all the doctype names for Ballot
+
+ELECTION = "Election"
+TEAM = "Election Team"
+TEAM_MEMBER = "Election Team Member"
+
+CANDIDATE = "Election Candidate"
+VOTE = "Candidate Vote"
+
+NOMINATION_FORM = "Election Nomination Form"
+CANDIDATE_APPLICATION = "Election Candidate Application"
