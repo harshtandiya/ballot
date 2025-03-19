9c3a908414c682034b9accbe3c914f2d3d37b137
Harsh Tandiya <harsh.tandiya@gmail.com>
2025-03-19 23:25:15 +0530

    add: candidate vote tier child table doctype

---
+++ ballot/election/doctype/candidate_vote_tier/candidate_vote_tier.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Harsh Tandiya and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class CandidateVoteTier(Document):
+    pass
