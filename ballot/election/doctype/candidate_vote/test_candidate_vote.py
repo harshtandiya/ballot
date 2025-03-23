9d9e569c9974a65cb042ef3e0f6e1bfca5206b5f
Harsh Tandiya <harsh.tandiya@gmail.com>
2025-03-23 22:14:43 +0530

    test: add testing for voting

--- ballot/election/doctype/candidate_vote/test_candidate_vote.py
+++ ballot/election/doctype/candidate_vote/test_candidate_vote.py
@@ -5,152 +5,132 @@ import frappe
 from faker import Faker
 from frappe.tests import IntegrationTestCase
 
+from ballot.id.doctypes import (
+    CANDIDATE,
+    CANDIDATE_APPLICATION,
+    ELECTION,
+    NOMINATION_FORM,
+    TEAM,
+    VOTE,
+)
+from ballot.tests.utils import (
+    create_candidate_application,
+    create_election,
+    create_election_team,
+    create_nomination_form,
+)
+
 fake = Faker()
 
-ELECTION = "Election"
-TEAM = "Election Team"
-CANDIDATE_DOC = "Election Candidate"
-FORM = "Election Nomination Form"
-APPLICATION_DOC = "Election Candidate Application"
-VOTE_DOC = "Candidate Vote"
+
 TEAM_OWNER = "test1@example.com"
-CANDIDATE_1 = "test2@example.com"
-CANDIDATE_2 = "test3@example.com"
+CANDIDATE_USER = "test2@example.com"
+VOTER = "test3@example.com"
 
 
 class TestCandidateVote(IntegrationTestCase):
     def setUp(self):
         frappe.set_user(TEAM_OWNER)
-
-        # Create a Team
-        team = frappe.get_doc(
-            {
-                "doctype": TEAM,
-                "team_name": fake.name(),
-            }
+        self.election_team = create_election_team()
+        self.election = create_election(
+            "Test Election", self.election_team.name, voting_status="Live"
         )
-        team.insert()
-        self.team = team
-
-        # Create an Election, linked to the above team
-        election = frappe.get_doc(
-            {
-                "doctype": ELECTION,
-                "title": fake.name(),
-                "organizing_team": self.team.name,
-            }
+        self.nomination_form = create_nomination_form(self.election, status="Live")
+        frappe.set_user(CANDIDATE_USER)
+        self.candidate_application = create_candidate_application(
+            self.election.name, self.nomination_form.name
         )
-        election.insert()
-        self.election = election
-
-        # Create a Nomination Form for the election
-        form = frappe.get_doc(
+        self.candidate = frappe.get_doc(
+            CANDIDATE,
             {
-                "doctype": FORM,
-                "status": "Live",
+                "linked_application": self.candidate_application.name,
                 "election": self.election.name,
-            }
-        )
-        form.insert()
-        self.form = form
-
-        # Create a 1st Candidate Application for the election
-        frappe.set_user(CANDIDATE_1)
-        application_1 = frappe.get_doc(
-            {
-                "doctype": APPLICATION_DOC,
-                "user": CANDIDATE_1,
-                "election": self.election.name,
-                "nomination_form": form.name,
-                "full_name": fake.name(),
-                "email": fake.email(),
-            }
-        )
-        application_1.insert()
-
-        # Create a 2nd Candidate Application for the election
-        frappe.set_user(CANDIDATE_2)
-        application_2 = frappe.get_doc(
-            {
-                "doctype": APPLICATION_DOC,
-                "user": CANDIDATE_2,
-                "election": self.election.name,
-                "nomination_form": form.name,
-                "full_name": fake.name(),
-                "email": fake.email(),
-            }
-        )
-        application_2.insert()
-
-        # Accept the applications
-        frappe.set_user(TEAM_OWNER)
-        application_1.status = "Accepted"
-        application_1.save()
-
-        application_2.status = "Accepted"
-        application_2.save()
-
-        self.application_1 = application_1
-        self.application_2 = application_2
-
-        frappe.set_user("Administarator")
-
-        # Get Candidate Docs
-        candidate_1 = frappe.get_doc(
-            CANDIDATE_DOC,
-            {
-                "election": self.election.name,
-                "linked_application": self.application_1.name,
             },
         )
-        candidate_2 = frappe.get_doc(
-            CANDIDATE_DOC,
-            {
-                "election": self.election.name,
-                "linked_application": self.application_2.name,
-            },
-        )
-
-        self.candidate_1 = candidate_1
-        self.candidate_2 = candidate_2
 
     def tearDown(self):
         frappe.set_user("Administrator")
-        frappe.delete_doc(TEAM, self.team.name, force=1)
+        frappe.delete_doc(TEAM, self.election_team.name, force=True)
         frappe.delete_doc(ELECTION, self.election.name, force=1)
-        frappe.delete_doc(FORM, self.form.name, force=1)
-        frappe.delete_doc(APPLICATION_DOC, self.application_1.name, force=1)
-        frappe.delete_doc(APPLICATION_DOC, self.application_2.name, force=1)
-        frappe.delete_doc(CANDIDATE_DOC, self.candidate_1.name, force=1)
-        frappe.delete_doc(CANDIDATE_DOC, self.candidate_2.name, force=1)
+        frappe.delete_doc(NOMINATION_FORM, self.nomination_form.name, force=1)
+        frappe.delete_doc(CANDIDATE_APPLICATION, self.candidate_application.name, force=1)
+        frappe.delete_doc(CANDIDATE, self.candidate.name, force=1)
 
-    def test_single_vote(self):
-        # Given a candidate vote is created for an election
-        # When the user tries to vote for another candidate in the same election
-        # Then the user should not be able to vote for another candidate
+    def test_single_candidate_vote(self):
+        frappe.set_user(VOTER)
 
-        frappe.set_user("test4@example.com")
-        vote1 = frappe.get_doc(
+        candidate_vote = frappe.get_doc(
             {
-                "doctype": VOTE_DOC,
+                "doctype": VOTE,
                 "vote_by": frappe.session.user,
                 "election": self.election.name,
-                "candidate": self.candidate_1.name,
+                "candidate_tiers": [
+                    {
+                        "rank": 1,
+                        "candidate": self.candidate.name,
+                    }
+                ],
             }
         )
-        vote1.insert()
+        candidate_vote.submit()
 
-        vote2 = frappe.get_doc(
+        # Given a Voter has already voted for a Candidate, they should not be able to vote again
+        second_vote = frappe.get_doc(
             {
-                "doctype": VOTE_DOC,
+                "doctype": VOTE,
                 "vote_by": frappe.session.user,
                 "election": self.election.name,
-                "candidate": self.candidate_2.name,
+                "candidate_tiers": [
+                    {
+                        "rank": 1,
+                        "candidate": self.candidate.name,
+                    }
+                ],
+            }
+        )
+        with self.assertRaises(frappe.ValidationError):
+            second_vote.submit()
+
+    def test_valid_user_vote(self):
+        frappe.set_user(VOTER)
+
+        candidate_vote = frappe.get_doc(
+            {
+                "doctype": VOTE,
+                "vote_by": "not_a_valid_user@example.com",
+                "election": self.election.name,
+                "candidate_tiers": [
+                    {
+                        "rank": 1,
+                        "candidate": self.candidate.name,
+                    }
+                ],
             }
         )
 
         with self.assertRaises(frappe.ValidationError):
-            vote2.insert()
+            candidate_vote.submit()
 
-        frappe.set_user('Administrator')
-        vote1.delete(force=1)
+    def test_voting_when_voting_is_closed(self):
+        frappe.set_user(TEAM_OWNER)
+        self.election.voting_status = "Closed"
+        self.election.save()
+
+        frappe.set_user(VOTER)
+
+        candidate_vote = frappe.get_doc(
+            {
+                "doctype": VOTE,
+                "vote_by": frappe.session.user,
+                "election": self.election.name,
+                "candidate_tiers": [
+                    {
+                        "rank": 1,
+                        "candidate": self.candidate.name,
+                    }
+                ],
+            }
+        )
+
+        with self.assertRaises(frappe.ValidationError):
+            candidate_vote.submit()
--- ballot/tests/utils.py
+++ ballot/tests/utils.py
@@ -1,10 +1,12 @@
 import frappe
 from faker import Faker
 
+from ballot.id.doctypes import CANDIDATE, CANDIDATE_APPLICATION, ELECTION, NOMINATION_FORM, TEAM
+
 fake = Faker()
 
 
-def create_election_team():
+def create_election_team() -> dict:
     """
     Creates and returns an Election Team.
 
@@ -12,7 +14,7 @@ def create_election_team():
     """
     election_team = frappe.get_doc(
         {
-            "doctype": "Election Team",
+            "doctype": TEAM,
             "team_name": fake.name(),
         }
     )
@@ -21,16 +23,71 @@ def create_election_team():
     return election_team
 
 
-def create_election(title, team_name):
+def create_election(title: str, team_id: str, **kwargs) -> dict:
     """Creates and returns an Election linked to an Election Team."""
     election = frappe.get_doc(
         {
-            "doctype": "Election",
+            "doctype": ELECTION,
             "title": title,
-            "organizing_team": team_name,
-            "status": "Draft",
+            "organizing_team": team_id,
+            "status": kwargs.get("status", "Draft"),
+            "voting_status": kwargs.get("voting_status", "Unopened"),
+            "allowed_preference_count": kwargs.get("allowed_preference_count", 1),
         }
     )
     election.insert()
 
     return election
+
+
+def create_nomination_form(election, **kwargs) -> dict:
+    """Creates and returns a Nomination Form linked to an Election."""
+    nomination_form = frappe.get_doc(
+        {
+            "doctype": NOMINATION_FORM,
+            "election": election.name,
+            "status": kwargs.get("status", "Draft"),
+            "accept_incoming_applications": kwargs.get("accept_incoming_applications", 1),
+        }
+    )
+    nomination_form.insert()
+
+    return nomination_form
+
+
+def create_candidate_application(election_id: str, form_id: str, **kwargs) -> dict:
+    """Creates and returns a Candidate Application linked to a Nomination Form.
+
+    Note: session user is auto added as a candidate
+    """
+
+    candidate_application = frappe.get_doc(
+        {
+            "user": kwargs.get("user", frappe.session.user),
+            "doctype": CANDIDATE_APPLICATION,
+            "election": election_id,
+            "nomination_form": form_id,
+            "status": kwargs.get("status", "Pending"),
+            "full_name": fake.name(),
+            "email": fake.email(),
+            "designation": fake.job(),
+            "organization": fake.company(),
+        }
+    )
+    candidate_application.insert()
+
+    return candidate_application
+
+
+def create_candidate(election_id: str, application_id: str) -> dict:
+    """Creates and returns a Candidate linked to a Candidate Application."""
+    candidate = frappe.get_doc(
+        {
+            "doctype": CANDIDATE,
+            "election": election_id,
+            "linked_application": application_id,
+        }
+    )
+    candidate.insert()
+
+    return candidate
